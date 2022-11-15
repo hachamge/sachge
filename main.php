f37051bd678cb1322914cc65a0b062d5652d314d
Seymour Roberts <seymour2@pdx.edu>
2022-11-15 10:33:41 -0800

    rename Diagram.php to Url_properties

--- main.php
+++ main.php
@@ -4,12 +4,11 @@
 </head>
 <body>
 <?php
+	require_once ("Html.php");
 	require_once ("Diagram.php");
 	
 	$Url = new Url();
-	$Url->reference = "Etz Hayim";
-	$node = new Url_node($Url);	
-	$node->render();
+	$Url->render();
 ?>
 </body>
 </html>
