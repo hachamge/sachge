4cfce92d24e66bbbffe3f90cc01712e644315dc3
Seymour Roberts <seymour2@pdx.edu>
2022-11-14 17:47:39 -0800

    refactor UrlSearch.php file

--- UrlSearch.php
+++ UrlSearch.php
@@ -1,4 +1,5 @@
 <?php
+	require_once ("Url.php");
 	require_once ("Html.php");
 	require_once ("image.php");
 	require_once ("ElementUtils.php");
@@ -25,29 +26,40 @@
 		}
 
 		private function createSearchTags():Div {
-			$EUtil_arr = [EUtil::div, EUtil::p, EUtil::p,EUtil::p,EUtil::p,EUtil::p];
+			$EUtil_arr = [EUtil::div, EUtil::p, EUtil::p,EUtil::p,EUtil::p,EUtil::p,EUtil::p];
 			$E_arr = ElementUtils::createElements($EUtil_arr);
-			$E_arr[0]->dset(2);
+			$E_arr[0]->dset(3);
 			$E_arr[0]->cset("UrlSearchTags");
-			$E_arr[1]->iset("pointer")->innerHtml("github.com/");
-			$E_arr[2]->iset("pointer")->innerHtml("Etz Hayim");
-			$E_arr[3]->iset("pointer")->innerHtml("seeq.com/");
-			$E_arr[4]->iset("pointer")->innerHtml("Node.js");
-			$E_arr[5]->iset("pointer")->innerHtml("OpenJs");
-			$E_arr[0]->inject($E_arr[1]);
-			$E_arr[0]->inject($E_arr[2]);
-			$E_arr[0]->inject($E_arr[3]);
-			$E_arr[0]->inject($E_arr[4]);
-			$E_arr[0]->inject($E_arr[5]);
+			for ($ind_K = 1; $ind_K <= 6; $ind_K++) 
+				$E_arr[$ind_K]->iset("pointer")->innerHtml(input::randomColor());
+			for ($ind_K = 1; $ind_K <= 6; $ind_K++) $E_arr[0]->inject($E_arr[$ind_K]);
+
 			return $E_arr[0];
 		}
 
 		private function config ():void {
 			$div = new Div("searchFrame");
+			$div->inject($this->config_cHash());
 			$div->inject($this->createSearchBtns());
 			$div->inject($this->createSearchTags());
 
 			$this->searchFrame->inject($div);
+			$this->config_radio();
+			#$this->config_cHash();
+		}
+		private function config_radio():void {
+			$degree = [1,2,3];
+			$descriptor = ["find post before: ", "end search for date: ", "end search for date: "];
+			$radio = [EUtil::time, EUtil::time, EUtil::time];
+			$div = Url::createSelection($radio,$descriptor,$degree);
+			$div->cset("advanceSearch");
+			$this->searchFrame->inject($div);
+		}
+		private function config_cHash():Div {
+			$cDiv = ElementUtils::createColors(5);
+			$cDiv->dset(2);
+			$cDiv->cset("chash");
+			return $cDiv;
 		}
 
 
