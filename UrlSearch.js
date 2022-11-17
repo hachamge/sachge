9108fce9f39947be0928646add209780804b75f4
Seymour Roberts <seymour2@pdx.edu>
2022-11-17 14:16:25 -0800

    enable regex search

--- UrlSearch.js
+++ UrlSearch.js
@@ -1,21 +1,25 @@
-// search through all the Url Descriptor, and highlight search results
-document.querySelector("#hsearch").addEventListener("click", function(){
+// search every Url for a match
+document.querySelector("#hsearch").addEventListener("change", function(){
 	// get the search query and remove white space
-	const search = document.querySelector("#iframeSearch").value.trim();
+	const search = this.value.trim();
+	if (search == "") return;
+	
+	const tbody = document.getElementsByTagName("tbody");
+	const tr = document.getElementsByTagName("tr");
+	const size = tr.length;
 
-	// search every Url descriptor: for the search input
-	const iframes = Array.from(document.querySelectorAll("#Descriptor"));
-	iframes.forEach(function(item){
-		const text = item.innerHTML;
+	for (var i = 1; i < size; i++) {
+		const td = tr[i].children;
+		const text = td[3].children[0].innerHTML;
+
 		const regx = new RegExp(search,"gi");
 		const result = text.replace(regx, `<mark>${search}</mark>`);
-		item.innerHTML = result;
-	});
+		td[3].children[0].innerHTML = result;
+	}
 }, true);
 
 // search for UrlDirectories 
 document.querySelector("#searchDir").addEventListener("keyup", function(){
-	const table = document.querySelector("#Url_index");
 	const tbody = document.getElementsByTagName("tbody");
 	const tr = document.getElementsByTagName("tr");
 	const size = tr.length;
@@ -30,10 +34,14 @@ function searchDir_tr(tr, DirSearch) {
 	const checkbox = td[5].children[0];
 	const td_Dir = td[4].children[0].innerHTML;
 
-	if (DirSearch == td_Dir) {
-		checkbox.checked = true;
-		tr.style.filter = "blur(0)";
-	} 
+	if (DirSearch == td_Dir) checkbox.checked = true;
 	else if (DirSearch == "") tr.style.display = "block";
 	else if (DirSearch != td_Dir) checkbox.checked = false;
+}
+
+function hsearch(hinput) {
+	const text = hinput.innerHTML;
+	const regx = new RegExp(search,"gi");
+	const result = text.replace(regx, `<mark>${search}</mark>`);
+	item.innerHTML = result;
 }
